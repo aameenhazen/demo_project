7c681203733dbb6676c9a95922cc4ae49c6fcf4a
Abdul Rehman Ameen <aameen@hazen.ai>
2025-06-19 12:54:44 +0500

    changed loop from 0 to 9 --> 1 to 10

--- hello-world.py
+++ hello-world.py
@@ -3,6 +3,6 @@ print('hello-world again!')
 print('hello-world by master!')
 print('hello-world by master-2!')
 #master
-master_list = [count for count in range(10)]
+master_list = [count for count in range(1, 11)]
 print(master_list)
 #master
