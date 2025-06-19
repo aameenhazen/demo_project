01757ad8c4e1b17a3b9df3baef63d29c52881624
Abdul Rehman Ameen <aameen@hazen.ai>
2025-06-19 11:45:45 +0500

    3 lines addes by extra

--- hello-world.py
+++ hello-world.py
@@ -2,3 +2,9 @@ print('hello-world!')
 print('hello-world again!')
 print('hello-world by master!')
 print('hello-world by master-2!')
+#extra
+a = 45
+b = a + 30
+print(f'value of b: {b}')
+#extra
+
